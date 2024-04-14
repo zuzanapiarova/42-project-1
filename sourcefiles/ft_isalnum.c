b6fd65becda092586bc77201245595f6307ecfcf
Zuzana Piarová <zuzana.piarova1@gmail.com>
2024-04-14 15:43:39 +0200

    finished isalnum

--- sourcefiles/ft_isalnum.c
+++ sourcefiles/ft_isalnum.c
@@ -6,11 +6,13 @@
 /*   By: zuzanapiarova <zuzanapiarova@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 16:31:19 by tomas             #+#    #+#             */
-/*   Updated: 2024/04/14 12:59:57 by zuzanapiaro      ###   ########.fr       */
+/*   Updated: 2024/04/14 15:43:23 by zuzanapiaro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <ctype.h>
+#include <stdio.h>
 
 int	ft_isalnum(int c)
 {
@@ -18,3 +20,8 @@ int	ft_isalnum(int c)
 		return (1);
 	return (0);
 }
+// int main(void)
+// {
+// 	int test = 97;
+// 	printf("isalnum: %d", ft_isalnum(test));
+// }
