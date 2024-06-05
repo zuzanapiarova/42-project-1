aefd61f471b04453530d991d1117776baf461aa4
Zuzana Piarová <zuzana.piarova1@gmail.com>
2024-06-05 17:43:16 +0200

    only fix strlcat and strrchr has segfault

--- sourcefiles/ft_strlcat.c
+++ sourcefiles/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: zuzanapiarova <zuzanapiarova@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 19:35:16 by tomas             #+#    #+#             */
-/*   Updated: 2024/06/02 14:01:08 by zuzanapiaro      ###   ########.fr       */
+/*   Updated: 2024/06/04 17:06:34 by zuzanapiaro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,39 +20,100 @@ size_t ft_strlcat(char *dst, const char *src, size_t size)
 	size_t j;
 	size_t dest_size;
 	size_t src_size;
+	size_t free_space;
 
-	i = 1;
+	dest_size = strlen(dst);
+	i = dest_size;
+	src_size = strlen(src);
 	j = 0;
-	while(dst[i] != '\0')
-	{
-		i++;
-	}
-	dest_size = i;
-	while(src[j] && j <= size - dest_size)
-	{
-		dst[i] = src[i];
-		i++;
-		j++;
-	}
-	src_size = j;
+
 	if(size == 0)
 	{
+		printf("printing 0\n");
+		dst[dest_size] = '\0';
 		return src_size;
 	}
-	if(dest_size >= size)
+	else if(size < dest_size)
 	{
+		printf("printing 1\n");
 		dst[size] = '\0';
-		return (dest_size + size);
+		return size + src_size;
 	}
+	else if(size == 1)
+	{
+		printf("printing 2\n");
+		return (src_size);
+	}
+	else if(size == 1 && dst[0] == 0)
+	{
+		printf("printing 3\n");
+		dst[0] = '\0';
+		return src_size;
+	}
+	if(size > dest_size + src_size + 1)
+		free_space = size - dest_size - src_size - 1;
+	else
+		free_space = size - dest_size - 1;
+	while(src[j] && free_space)
+	{
+			dst[i] = src[j];
+			i++;
+			j++;
+			free_space--;
+	}
+	printf("printing 4\n");
 	dst[i] = '\0';
 	return dest_size + src_size;
 }
-/* int main(void)
-{
-	char *src = "World";
-	char dest[20] = "Hello ";
-	unsigned int size = 3;
-	printf("my func: %s\n", ft_strncat(dest, src, size));
-	//printf("strcat: %s\n", strncat(dest, src, size));
-	return(0);
-} */
+
+// int main(void)
+// {
+// 	char dest[30];
+// 	//memset(dest, 0, 30);
+// 	dest[0] = 'B';
+// 	//memset(dest, 'B', 4);
+// 	char * src = (char *)"AAAAAAAAA";
+// 	//dest[0] = 'B';
+// 	printf("%s", dest);
+// 	printf("my func: %ld\n", ft_strlcat(dest, "123", 0));
+// 	printf("%s", dest);
+// 	return(0);
+// }
+
+	// memset(dest, 0, 30);
+	//////////// /* 17 */ check(ft_strlcat(dest, "123", 0) == 3 && !strcmp(dest, "")); showLeaks();
+	// dest[0] = 'B';
+	////////////// /* 2 */ check(ft_strlcat(dest, src, 1) == 10 && !strcmp(dest, "B")); showLeaks();
+
+	// /* 1 */ check(ft_strlcat(dest, src, 0) == strlen(src) && !strcmp(dest, "B")); showLeaks();
+	// memset(dest, 'B', 4);
+	// /* 3 */ check(ft_strlcat(dest, src, 3) == 3 + strlen(src) && !strcmp(dest, "BBBB")); showLeaks();
+	// /* 4 */ check(ft_strlcat(dest, src, 6) == 13 && !strcmp(dest, "BBBBA")); showLeaks();
+	// if(size > dest_size && size < dest_size + src_size)
+
+	// memset(dest, 'C', 5);
+	// /* 5 */ check(ft_strlcat(dest, src, -1) == 14 && !strcmp(dest, "CCCCCAAAAAAAAA")); showLeaks();
+	// memset(dest, 'C', 15);
+	// /* 6 */ check(ft_strlcat(dest, src, 17) == 24 && !strcmp(dest, "CCCCCCCCCCCCCCCA")); showLeaks();
+	// memset(dest, 0, 30);
+	////////////
+	//* 7 */ check(ft_strlcat(dest, src, 1) == strlen(src) && !strcmp(dest, "")); showLeaks();
+	// memset(dest, 0, 30); memset(dest, '1', 10);
+	// /* 8 */ check(ft_strlcat(dest, src, 5) == strlen(src) + 5 && !strcmp(dest, "1111111111")); showLeaks();
+	// memset(dest, 0, 30); memset(dest, '1', 10);
+	// /* 9 */ check(ft_strlcat(dest, src, 5) == strlen(src) + 5 && !strcmp(dest, "1111111111")); showLeaks();
+	// memset(dest, 0, 30); memset(dest, '1', 10);
+	// /* 10 */ check(ft_strlcat(dest, "", 15) == 10 && !strcmp(dest, "1111111111")); showLeaks();
+	// memset(dest, 0, 30);
+	// /* 11 */ check(ft_strlcat(dest, "", 42) == 0 && !strcmp(dest, "")); showLeaks();
+	// memset(dest, 0, 30);
+	// /* 12 */ check(ft_strlcat(dest, "", 0) == 0 && !strcmp(dest, "")); showLeaks();
+	// memset(dest, 0, 30);
+	/////////// /* 13 */ check(ft_strlcat(dest, "123", 1) == 3 && !strcmp(dest, "")); showLeaks();
+	// memset(dest, 0, 30);
+	// /* 14 */ check(ft_strlcat(dest, "123", 2) == 3 && !strcmp(dest, "1")); showLeaks();
+	// memset(dest, 0, 30);
+	// /* 15 */ check(ft_strlcat(dest, "123", 3) == 3 && !strcmp(dest, "12")); showLeaks();
+	// memset(dest, 0, 30);
+	// /* 16 */ check(ft_strlcat(dest, "123", 4) == 3 && !strcmp(dest, "123")); showLeaks();
+	// write(1, "\n", 1);
--- sourcefiles/ft_strnstr.c
+++ sourcefiles/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: zuzanapiarova <zuzanapiarova@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 21:44:00 by zuzanapiaro       #+#    #+#             */
-/*   Updated: 2024/06/02 18:43:09 by zuzanapiaro      ###   ########.fr       */
+/*   Updated: 2024/06/05 17:42:01 by zuzanapiaro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,65 +14,35 @@
 #include <stdio.h>
 #include "libft.h"
 
-char *ft_strnstr(const char *big, const char *little, size_t len)
-{
-	//char *result;
-	int i;
-	int j;
+char *ft_strnstr(const char *haystack, const char *needle, size_t len) {
+	size_t i;
+    size_t needle_len;
 
 	i = 0;
-	j = 0;
-	if(*little == '\0')
-		return ((char *) big);
-	while(big[i] && len > 0)
-	{
-		if(little[0] == big[i])
-		{
-			//result = (char *)big + i;
-			while(little[j] == big[i])
-			{
-				i++;
-				j++;
-			}
-		}
+    needle_len = strlen(needle);
+    if (*needle == '\0')
+        return (char *)haystack;
+	while(i <= len)
+    {
+        // Ensure not to go past the end of haystack
+        if (needle_len + i > len)
+            return NULL;
+
+        // Check if needle matches the substring starting at position i
+        if (strncmp(haystack + i, needle, needle_len) == 0)
+            return (char *)(haystack + i);
+
+        // Stop if we've reached the end of haystack
+        if (haystack[i] == '\0')
+            return NULL;
 		i++;
-		len--;
-	}
-	return (NULL);
+    }
+	return NULL;
 }
 
 // int main(void)
 // {
-// 	char *big = "ababcdef";
-// 	char *little = "abc";
-// 	printf("%s\n", ft_strnstr(big, little, 10));
+// 	char haystack[30] = "aaaabcaaaaaabcd";
+// 	char needle[10] = "abc";
+// 	printf("%s\n", ft_strnstr(haystack, needle, 10));
 // }
-
-
-/* {
-	size_t		il;
-	size_t		i;
-	const char *temp;
-
-	i = 0;
-	il = ft_strlen(little);
-	printf("%ld\n", il);
-	if (*little == '\0')
-		return ((char *)big);
-	while (*big && (len != 0))
-	{
-		if (*big == *little)
-		{
-			i++;
-			if (i == 1)
-				temp = big;
-			little++;
-		}
-		else if (i == il)
-			return ((char *)temp);
-		else
-			i = 0;
-		big++;
-	}
-	return (NULL);
-} */
